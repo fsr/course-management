c5ec4a97419ec598460507bcc7dab6c965168566
Justus Adam <dev@justus.science>
2015-11-08 00:15:49 +0100

    Added query for active subjects.

--- src/course_management/views/base.py
+++ src/course_management/views/base.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
 from course_management.models import course, subject
+from django.db.models import Count
 import collections
 
 
-def get_all_courses():
-    return course.Course.objects.filter(active=True)
-
-def get_all_subjects():
-    return subject.Subject.objects.all()
+def get_relevant_subjects():
+    return subject.Subject.objects.annotate(course_count=Count('course')).filter(course_count__gt=0)
 
 def render_with_default(request, template, context):
     default_context = {
-         'active_courses': get_all_courses(),
-         'active_subjects': get_all_subjects()
+         'active_subjects': get_relevant_subjects()
     }
     return render(request, template, collections.ChainMap(context, default_context))
