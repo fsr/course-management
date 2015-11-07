6dfd07b125c1ec63dc0702945294591b340ac582
Felix Wittwer <felix.wittwer@gmail.com>
2015-11-07 19:50:13 +0100

    validation in registration

--- src/course_management/views/register.py
+++ src/course_management/views/register.py
@@ -2,9 +2,17 @@ from course_management.views.base import render_with_default
 from course_management.forms import RegistrationForm
 
 
+
 def register(request):
 
-    form = RegistrationForm()
-
-    return render_with_default(request, 'register.html', {'title': 'Registration | iFSR Course Management',
-                                                          'form': form})
+    if request.method == 'POST':
+        # print(request.POST)
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            print("YES")
+        else:
+            print("NO")
+    else:
+        form = RegistrationForm()
+        return render_with_default(request, 'register.html', {'title': 'Registration | iFSR Course Management',
+                                                              'form': form})
