6ccdd34fc6fe2baeb5b7075be58d23c8a32fff9d
Justus Adam <dev@justus.science>
2015-11-08 12:16:50 +0100

    Moved query.

--- src/course_management/models/subject.py
+++ src/course_management/models/subject.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.db.models import Count
 
 
 class Subject(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField(default="")
 
-
     def __str__(self):
         return self.name
+
+    @classmethod
+    def get_active(cls):
+        return cls.objects.annotate(course_count=Count('course')).filter(course_count__gt=0)
--- src/course_management/views/base.py
+++ src/course_management/views/base.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-from course_management.models import course, subject
-from django.db.models import Count
+from course_management.models import subject
 import collections
 
 
-def get_relevant_subjects():
-    return subject.Subject.objects.annotate(course_count=Count('course')).filter(course_count__gt=0)
-
 def render_with_default(request, template, context):
     default_context = {
-         'active_subjects': get_relevant_subjects()
+         'active_subjects': subject.Subject.get_active()
     }
     return render(request, template, collections.ChainMap(context, default_context))
