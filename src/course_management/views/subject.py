3dc5a667ff33cfa9e382e90ae5c2cb49f0a465fc
manuelthieme <manuelthieme@googlemail.com>
2015-11-08 15:15:03 +0100

    removed backbutton from course owerview

--- src/course_management/views/subject.py
+++ src/course_management/views/subject.py
@@ -1,5 +1,4 @@
 from course_management.views.base import render_with_default
-from django.core.urlresolvers import reverse
 
 from course_management.models import course, subject
 
@@ -8,5 +7,4 @@ def render_course_overview(request, subjectname):
     active_subject = subject.Subject.objects.filter(name=subjectname).get()
     return render_with_default(request, 'subject.html', {'title': '{name} | iFSR Course Management'.format(name=subjectname),
                                                          'subject': active_subject,
-                                                         'backurl': reverse('index'),
                                                          })
