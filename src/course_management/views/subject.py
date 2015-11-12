8197ccb34213d69e0667bbbcafd8483301f981c4
manuelthieme <manuelthieme@googlemail.com>
2015-11-12 18:35:17 +0100

    course table templat

--- src/course_management/views/subject.py
+++ src/course_management/views/subject.py
@@ -7,4 +7,5 @@ def course_overview(request, subjectname):
     active_subject = subject.Subject.objects.filter(name=subjectname).get()
     return render_with_default(request, 'subject.html', {'title': '{name} | iFSR Course Management'.format(name=subjectname),
                                                          'subject': active_subject,
+                                                         'course_list': active_subject.course_set.all,
                                                          })
