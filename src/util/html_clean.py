fe996a4bb7b40747cc8868e5c63ac972728c84d0
Justus Adam <dev@justus.science>
2015-11-14 22:49:24 +0100

    Fixed an error with html escaping

--- src/util/html_clean.py
+++ src/util/html_clean.py
@@ -6,7 +6,7 @@ USER_DESCR_ALLOWED_TAGS = bleach.ALLOWED_TAGS + ['h2', 'h3', 'h4', 'h5', 'h6']
 
 
 def clean_for_user_description(html):
-    return bleach.clean(html, tags=DESCR_ALLOWED_TAGS, strip=True)
+    return bleach.clean(html, tags=USER_DESCR_ALLOWED_TAGS, strip=True)
 
 
 def clean_for_description(html):
