b1efc997c510fbdcaeb8d3ba9b4202ac810bb9ff
Felix Wittwer <dev@felixwittwer.de>
2016-06-23 15:13:46 +0200

    Allow <br /> and <p> in descriptions

--- util/html_clean.py
+++ util/html_clean.py
@@ -1,8 +1,8 @@
 import bleach
 
 
-DESCR_ALLOWED_TAGS = bleach.ALLOWED_TAGS + ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
-USER_DESCR_ALLOWED_TAGS = bleach.ALLOWED_TAGS + ['h2', 'h3', 'h4', 'h5', 'h6']
+DESCR_ALLOWED_TAGS = bleach.ALLOWED_TAGS + ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'br', 'p']
+USER_DESCR_ALLOWED_TAGS = bleach.ALLOWED_TAGS + ['h2', 'h3', 'h4', 'h5', 'h6', 'br', 'p']
 
 
 def clean_for_user_description(html):
