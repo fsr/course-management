fee4bc06efcee011e40c99d3c0a1bb10ee31826d
Felix Wittwer <dev@felixwittwer.de>
2018-07-18 09:18:26 +0200

    Require consent to privacy policy during signup

--- user/models.py
+++ user/models.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
 from markdown import markdown
 
 from util.html_clean import clean_for_user_description
+
+
+def privacy_policy_consented(consented):
+    if consented:
+        return
+    raise ValidationError(
+        _('You must agree to the privacy policy to use our services.'))
 
 
 class Faculty(models.Model):
@@ -16,7 +25,8 @@ class UserInformation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.TextField(default="")
     public_profile = models.BooleanField(default=False)
-    accepted_privacy_policy = models.BooleanField()
+    accepted_privacy_policy = models.BooleanField(
+        validators=[privacy_policy_consented])
 
     def __str__(self):
         return '{first} {last}'.format(first=self.user.first_name, last=self.user.last_name)
