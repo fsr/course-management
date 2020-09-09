0fd54147c3d9b11a742863249642086240deb264
Felix Wittwer <dev@felixwittwer.de>
2020-09-09 22:26:13 +0200

    Add new contact form w/o sender field

--- user/views/contact.py
+++ user/views/contact.py
@@ -5,14 +5,17 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 from django.utils.translation import ugettext as _
 
-from user.forms import ContactForm
+from user.forms import AbstractContactForm
 from util.error.reporting import db_error
 
+CONTACT_FOOTER = """
+-------------------
+This message has been sent via the Course Mangement System.
+Sent by: """
 
 @login_required
 @csrf_protect
 def contact_form(request: HttpRequest, user_id: int):
-
     try:
         user = User.objects.get(id=user_id)
     except User.DoesNotExist:
@@ -20,20 +23,20 @@ def contact_form(request: HttpRequest, user_id: int):
     assert isinstance(user, User)
 
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = AbstractContactForm(request.POST)
         if form.is_valid():
+            content = form.content + CONTACT_FOOTER + request.user.email
             user.email_user(
                 subject="[CM contact form] " + form.subject,
-                message=form.content,
-                from_email=form.cleaned_data['sender']
+                message=content
             )
             return redirect('index')
     else:
-        form = ContactForm()
+        form = AbstractContactForm()
 
     return render(
         request,
-        'contact-form.html',
+        'new_ui_foo/contact-form.html',
         {
             'form': form,
             'user': user
